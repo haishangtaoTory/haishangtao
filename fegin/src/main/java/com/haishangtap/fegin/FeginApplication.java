61a8f8f4be3685ec22c846b23658e61e0a27999a
wutao8 <wutao8@xin.com>
2018-03-06 11:20:33 +0800

    添加fegin

---
+++ fegin/src/main/java/com/haishangtap/fegin/FeginApplication.java
@@ -0,0 +1,12 @@
+package com.haishangtap.fegin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeginApplication.class, args);
+	}
+}
