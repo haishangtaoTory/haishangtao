4e737efc4b6627a8969a86beecb5fd5f8b762eef
wutao8 <wutao8@xin.com>
2018-03-06 10:42:31 +0800

    SpringCloudTest Git

---
+++ springcloudtest/src/main/java/com/haishangtao/springcloudtest/SpringcloudtestApplication.java
@@ -0,0 +1,12 @@
+package com.haishangtao.springcloudtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudtestApplication.class, args);
+	}
+}
